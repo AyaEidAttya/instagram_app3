80109788b5197f47ef0d937808e10adf8f4033af
aya <ayaaeid.72@gmail.com>
2023-04-20 02:30:38 +0200

    add app bar

--- instagram_app3/lib/responsive/mobile.dart
+++ instagram_app3/lib/responsive/mobile.dart
@@ -1,4 +1,5 @@
 
+import 'package:flutter/material.dart';
 import 'package:flutter/src/animation/animation_controller.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -11,24 +12,15 @@ class mobilescreen extends StatefulWidget {
   State<mobilescreen> createState() => _mobilescreenState();
 }
 
-class _mobilescreenState extends State<mobilescreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(vsync: this);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
+class _mobilescreenState extends State<mobilescreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+appBar: AppBar(
+  title: Text("mobile screen"),
+),
+    );
+  
+    
   }
 }
