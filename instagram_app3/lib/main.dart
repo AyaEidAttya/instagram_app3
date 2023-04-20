5a9adb92feaef4ab2e46b30abe7491d0c1bbbfd0
aya <ayaaeid.72@gmail.com>
2023-04-20 02:55:43 +0200

    debugShow

--- instagram_app3/lib/main.dart
+++ instagram_app3/lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const  MaterialApp(
+      debugShowCheckedModeBanner: false,
       home:responsiv(my_phone_screan: mobilescreen(), my_web_screen: webscreen(),),
     );
   }
