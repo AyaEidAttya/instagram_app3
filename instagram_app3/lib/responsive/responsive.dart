491717fc4a7eb34447c9350c46db0f6a3f72ae0e
aya <ayaaeid.72@gmail.com>
2023-04-20 02:14:54 +0200

    creat responsive file

---
+++ instagram_app3/lib/responsive/responsive.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class responsiv extends StatefulWidget {
+  
+  const responsiv({super.key});
+
+  @override
+  State<responsiv> createState() => _responsivState();
+}
+
+class _responsivState extends State<responsiv> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
