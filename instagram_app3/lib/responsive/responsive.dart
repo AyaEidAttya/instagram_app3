919d1f49a0008f75103428b62f7283b87f2b2c9f
aya <ayaaeid.72@gmail.com>
2023-04-20 02:17:24 +0200

    add constructor

--- instagram_app3/lib/responsive/responsive.dart
+++ instagram_app3/lib/responsive/responsive.dart
@@ -5,7 +5,7 @@ import 'package:flutter/src/widgets/placeholder.dart';
 class responsiv extends StatefulWidget {
   final my_phone_screan;
   final my_web_screen;
-  const responsiv({super.key});
+  const responsiv({super.key,required this.my_phone_screan,required this.my_web_screen});
 
   @override
   State<responsiv> createState() => _responsivState();
