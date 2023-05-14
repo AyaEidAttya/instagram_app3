5248b9147c2b5259c82799f7222ed84ee259a3c5
My Name <myemail@example.com>
2023-05-14 15:33:49 +0300

    add neam

--- instagram_app3/lib/screenes/home.dart
+++ instagram_app3/lib/screenes/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../shaird/colors.dart';
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -12,15 +13,17 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
+      appBar: AppBar(
         actions: [
-          IconButton(onPressed:(){}, icon: Icon(Icons.message_outlined)),
-           IconButton(onPressed:(){}, icon: Icon(Icons.logout)),
-
+          IconButton(onPressed: () {}, icon: Icon(Icons.message_outlined)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
         ],
         backgroundColor: mobileBackgroundColor,
-       // ignore: deprecated_member_use
-       title: Text("instgram" ,style: TextStyle(fontWeight: FontWeight.w100),),
+        // ignore: deprecated_member_use
+        title: Text(
+          "instgram",
+          style: TextStyle(fontWeight: FontWeight.w100),
+        ),
       ),
       body: Column(
         children: [
@@ -28,17 +31,15 @@ class _HomeState extends State<Home> {
             children: [
               CircleAvatar(
                 radius: 26,
-                backgroundImage: NetworkImage("https://1000logos.net/wp-content/uploads/2017/02/New-Instagram-logo.jpg"),
+                backgroundImage: NetworkImage(
+                    "https://th.bing.com/th/id/OIP.h8_4I67bt6pTA-y_h2hzHwAAAA?pid=ImgDet&rs=1"),
               ),
-SizedBox(
-  width: 17,
-)
-
+              SizedBox(
+                width: 17,
+              ),
+              Text("aya_eid",style: TextStyle(fontSize: 15),)
             ],
-            
           ),
-
-
         ],
       ),
     );
